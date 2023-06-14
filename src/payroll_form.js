77b6e96f400ee776441609e952a90af317a2d26c
ks0510 <kshaikh051999@gmail.com>
2023-06-14 13:45:27 +0530

    Added html code to reset the form

--- src/payroll_form.js
+++ src/payroll_form.js
@@ -145,28 +145,28 @@ function createAndUpdateStorage(employeePayrollData) {
     localStorage.setItem("Employee PayrollList", JSON.stringify(employeePayrollList))
     } 
 
-    const resetForm = () => {
-        setValue( '#name','');
-        unsetSelectedValues('[name=profile]');
-        unsetSelectedValues('[name=gender]');
-        unsetSelectedValues('[name=department]');
-        setValue( '#salary','');
-        setValue( '#notes', '');
-        setValue( '#day', '1');
-        setValue('#month', 'January');
-        setValue( '#year', '2020');
-    }
-    const unsetSelectedValues = (propertyValue) => {
-        let allItems = document.querySelectorAll(propertyValue); 
-        allItems.forEach(item => {
-            item.checked = false;
+const resetForm = () => {
+    setValue( '#name','');
+    unsetSelectedValues('[name=profile]');
+    unsetSelectedValues('[name=gender]');
+    unsetSelectedValues('[name=department]');
+    setValue( '#salary','');
+    setValue( '#notes', '');
+    setValue( '#day', '1');
+    setValue('#month', 'January');
+    setValue( '#year', '2020');
+}
+const unsetSelectedValues = (propertyValue) => {
+    let allItems = document.querySelectorAll(propertyValue); 
+    allItems.forEach(item => {
+        tem.checked = false;
         });
     }
-    const setTextValue = (id, value) => {
-        const element = document.querySelector(id); 
-        element.textContent = value;
-    }
-    const setValue = (id, value) => {
-        const element = document.querySelector(id); 
-        element.value = value; 
-    }
+const setTextValue = (id, value) => {
+    const element = document.querySelector(id); 
+    element.textContent = value;
+}
+const setValue = (id, value) => {
+    const element = document.querySelector(id); 
+    element.value = value; 
+}
